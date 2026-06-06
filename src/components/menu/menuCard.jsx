113e62706d542bf60aa7a57d06f5db6be59af35c
slinnils <imbachnils@hotmail.ch>
2026-06-06 11:03:50 +0200

    optimized menu-navbar behaivour a little bit and better synthax

--- src/components/menu/menuCard.jsx
+++ src/components/menu/menuCard.jsx
@@ -14,8 +14,13 @@ export default function MenuCard({
     return formatter.format(price);
   }
 
+  function handleCategoryClick(cat, e) {
+    onSelectCategory(cat.title);
+    e.target.scrollIntoView({ inline: "center", block: "nearest", behavior: "smooth" });
+  }
+
   return (
-    <div className="my-12 flex flex-col items-center gap-5 mx-5 lg:w-[65vw] w-[95vw] max-w-350">
+    <section className="my-12 flex flex-col items-center gap-5 mx-5 lg:w-[65vw] w-[95vw] max-w-350">
       <div className="flex flex-col lg:flex-row lg:justify-center w-full max-w-350 bg-piz-card-bg shadow-lg py-5 px-5 sm:px-10 rounded-2xl gap-4">
         <button
           onClick={backToSelection}
@@ -37,22 +42,24 @@ export default function MenuCard({
           </svg>
           <span className="ml-3">Zurück zur Auswahl</span>
         </button>
-        <div className="nav-items flex gap-8 h-15 pr-20 overflow-x-scroll pb-2 lg:pb-0 nav-scroll">
+
+        <nav className="nav-items flex gap-8 h-15 pr-20 overflow-x-scroll pb-2 lg:pb-0 nav-scroll">
           {categories.map((cat) => (
             <div
               key={cat._id}
-              className="flex items-center"
+              className={`flex items-center order-[${cat.order}]`}
               style={{ order: cat.order }}
             >
               <button
-                onClick={() => onSelectCategory(cat.title)}
+                onClick={(e) => handleCategoryClick(cat, e)}
                 className={`cursor-pointer navigation rounded-2xl ${cat.title === selectedCategory.title ? "active" : "text-piz-text-bright"}`}
               >
                 {cat.title}
               </button>
             </div>
           ))}
-        </div>
+        </nav>
+
       </div>
 
       <div className="flex flex-col items-center gap-3">
@@ -65,11 +72,13 @@ export default function MenuCard({
         />
       </div>
 
-      <div className="bg-piz-card-bg shadow-lg md:px-10 px-5 py-3 rounded-2xl w-full">
+      <ul className="bg-piz-card-bg shadow-lg md:px-10 px-5 py-3 rounded-2xl w-full list-none">
         {item.map((meal) => (
-          <div key={meal._id} className="flex flex-col my-5">
+          <li key={meal._id} className="flex flex-col my-5">
             <div className="flex w-full justify-between">
-              <p className="text-[clamp(1.1rem,3vw,1.5rem)] text-piz-text-bright">{meal.title}</p>
+              <p className="text-[clamp(1.1rem,3vw,1.5rem)] text-piz-text-bright">
+                {meal.title}
+              </p>
               <span className="flex-1 mb-2 mx-2 border-b border-dotted border-piz-text-mute"></span>{" "}
               <p className="text-[clamp(1.25rem,3vw,1.5rem)] text-piz-accent self-end">
                 {priceConverter(meal.price)}
@@ -84,9 +93,9 @@ export default function MenuCard({
                 ⚠ {meal.allergy}
               </p>
             )}
-          </div>
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </section>
   );
 }
